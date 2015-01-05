5ac7f8d053382866da6342b3fb13e9d5925d45ac
Chris Ballinger <chrisballinger@gmail.com>
2015-01-05 07:53:52 -0800

    Initial Commit

---
+++ EncryptedFilesTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EncryptedFilesTest
+//
+//  Created by Christopher Ballinger on 1/5/15.
+//  Copyright (c) 2015 Chris Ballinger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EncryptedFilesTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EncryptedFilesTest
+//
+//  Created by Christopher Ballinger on 1/5/15.
+//  Copyright (c) 2015 Chris Ballinger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
